8d4fc3cdd8b9deef31296bcbdffc0042a49f5d1c
Sergei137 <sm1.618@protonmail.com>
2023-11-21 19:24:07 -0700

    small changes, xml files in resources are for testing only.

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,7 +3,8 @@ package main;
 public class Main {
     public static void main(String[] args) {
 
-
+        // read .xml sample files from command line
+        // sample files in tempResources folder are for testing purposes
 
     }
 }
