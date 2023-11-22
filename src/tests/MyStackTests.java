79625329680308198767f5f56600316e1dbaf158
Sergei137 <sm1.618@protonmail.com>
2023-11-21 18:50:37 -0700

    debugging junit import

---
+++ src/tests/MyStackTests.java
@@ -0,0 +1,28 @@
+package tests;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class MyStackTests {
+    @Test
+    public void testPush() {
+        MyStack stack = new MyStack();
+        stack.push(1);
+        assertEquals(1, stack.peek());
+    }
+
+    @Test
+    public void testPop() {
+        MyStack stack = new MyStack();
+        stack.push(1);
+        stack.pop();
+        assertEquals(0, stack.getSize());
+    }
+
+    @Test
+    public void testIsEmpty() {
+        MyStack stack = new MyStack();
+        assertEquals(true, stack.isEmpty());
+    }
+}
+
