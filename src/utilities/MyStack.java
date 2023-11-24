ee81a8bf094e1908d8b8a12b4f85b35bae6d55b7
Sergei137 <sm1.618@protonmail.com>
2023-11-24 00:09:04 -0700

    implemented MyArrayList into MyStack

--- src/utilities/MyStack.java
+++ src/utilities/MyStack.java
@@ -2,23 +2,18 @@ package utilities;
 
 import java.util.NoSuchElementException;
 
+import utilities.MyArrayList;
+
 public class MyStack<E> implements StackADT<E> {
-    private E[] data; // should this be final instead of private?
+    private MyArrayList<E> data; // should this be final instead of private?
     private int capacity; // should this be final instead of private?
     private int size;
 
-    // constructor 1
-    public MyStack() {
-        this.capacity = 10000;
-        data = (E[]) new Object[capacity];
-        size = 0;
-    }
-
-    // constructor 2
+    // constructor
     public MyStack(int capacity) {
         this.capacity = capacity;
-        data = (E[]) new Object[capacity];
-        size = 0;
+        this.data = new MyArrayList<>();
+        this.size = data.size();
     }
 
     // return capacity of stack
@@ -28,7 +23,7 @@ public class MyStack<E> implements StackADT<E> {
 
     // return size of stack
     public int getSize() {
-        return size;
+        return data.size();
     }
 
     // return true if stack is empty
@@ -38,29 +33,27 @@ public class MyStack<E> implements StackADT<E> {
 
     // add element to top of stack
     public void push(E toAdd) throws NullPointerException {
-        data[size] = toAdd;
+        data.add(toAdd);
         size++;
     }
 
     // return top element of stack without removing it
     public E peek () {
-        return data[size - 1];
+        return data.get(size - 1);
     }
 
     // remove top element of stack and return it
     public E pop () {
+        data.remove(size - 1);
         size--;
-        return data[size];
-    }
-
-    // return size of stack
-    public int size() {
-        return size;
+        return data.get(size - 1);
     }
 
     // remove all elements from stack
     public void clear() {
+        data.clear();
         size = 0;
+        capacity = 0;
     }
 
     // compare two stacks and return true if they are equal
@@ -82,7 +75,7 @@ public class MyStack<E> implements StackADT<E> {
 
         // if comparedStack is the same size as this stack, compare each element
         for (int i = 0; i < this.size; i++) {
-            if (!this.data[i].equals(thatStack.data[i])) {
+            if (!this.data.get(i).equals(thatStack.data.get(i))) {
                 return false;
             }
         }
@@ -92,12 +85,15 @@ public class MyStack<E> implements StackADT<E> {
     // return array of stack elements
     public Object[] toArray() {
         Object[] result = new Object[size];
-        System.arraycopy(data, 0, result, 0, size);
+        for (int i = 0; i < size; i++) {
+            result[i] = data.get(i);
+        }
         return result;
     }
 
     // return array of stack elements
     @Override
+    @SuppressWarnings("unchecked")
     public E[] toArray(E[] toHold) throws NullPointerException {
         if (toHold == null) {
             throw new NullPointerException("Input array cannot be null");
@@ -109,7 +105,9 @@ public class MyStack<E> implements StackADT<E> {
         }
 
         // Copy elements from data to toHold with same order
-        if (size >= 0) System.arraycopy(data, 0, toHold, 0, size);
+        for (int i = 0; i < size; i++) {
+            toHold[i] = data.get(i);
+        }
 
         // If toHold has more elements than the stack, set the additional elements to null
         for (int i = size; i < toHold.length; i++) {
@@ -126,7 +124,7 @@ public class MyStack<E> implements StackADT<E> {
         }
 
         for (int i = 0; i < size; i++) {
-            if (data[i].equals(toFind)) {
+            if (data.get(i).equals(toFind)) {
                 return true;
             }
         }
@@ -137,7 +135,7 @@ public class MyStack<E> implements StackADT<E> {
     public int search(E toFind) {
         // size - 1 sets i to top element of stack
         for (int i = size - 1; i >= 0; i--) {
-            if (data[i].equals(toFind)) {
+            if (data.get(i).equals(toFind)) {
                 return size - i;
             }
         }
@@ -162,7 +160,7 @@ public class MyStack<E> implements StackADT<E> {
                 if (!hasNext()) {
                     throw new NoSuchElementException("No more elements");
                 }
-                return data[index++];
+                return data.get(index++);
             }
         };
     }
