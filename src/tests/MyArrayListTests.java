d2145cb180637fff9cf4892119f3bfbb91bc565b
IuliaNeuc <Lulia.Neuc@edu.sait.ca>
2023-11-23 22:35:27 -0700

    Added comments to MyArrayList and MyArrayListTest code. Generated javadoc in the doc folder

--- src/tests/MyArrayListTests.java
+++ src/tests/MyArrayListTests.java
@@ -21,6 +21,8 @@ public class MyArrayListTests{
 
     @Test
     public void testSize(){
+
+        // Test the size method after adding elements to the list
         assertEquals(0, myList.size());
         myList.add("Element 1");
         assertEquals(1, myList.size());
@@ -30,6 +32,8 @@ public class MyArrayListTests{
 
     @Test
     public void testClear(){
+
+        // Test the clear method
         myList.add("Element 1");
         myList.add("Element 2");
         myList.clear();
@@ -40,6 +44,7 @@ public class MyArrayListTests{
     @Test
     public void testAddAtIndex(){
 
+        // Test adding an element at a specific index
         myList.add("Element1");
         myList.add("Element2");
         myList.add(1, "New Element");
@@ -50,11 +55,14 @@ public class MyArrayListTests{
 
     @Test(expected = IndexOutOfBoundsException.class)
     public void testAddAtIndexOutOfBounds(){
+        // Test adding an element at an out-of-bounds index, expecting an exception
         myList.add(1, "New Element");
     }
 
     @Test
     public void testAdd(){
+
+        // Test the add method
         assertTrue(myList.add("Element 1"));
         assertEquals(1, myList.size());
         assertEquals("Element 1", myList.get(0));
@@ -62,6 +70,8 @@ public class MyArrayListTests{
 
     @Test
     public void testAddAll(){
+
+        // Test adding all elements from another list
         ListADT<String> toAdd = new MyArrayList<>();
         toAdd.add("Elem 1");
         toAdd.add("Elem 2");
@@ -74,6 +84,8 @@ public class MyArrayListTests{
 
     @Test
     public void testGet(){
+
+        // Test the get method
         myList.add("Elem 1");
         myList.add("Elem 2");
         assertEquals("Elem 1", myList.get(0));
@@ -82,11 +94,14 @@ public class MyArrayListTests{
 
     @Test(expected = IndexOutOfBoundsException.class)
     public void testOutOfBounds(){
+        // Test getting an element from an empty list, expecting an exception
         myList.get(0);
     }
 
     @Test
     public void testRemoveAtIndex(){
+
+        // Test removing an element at a specific index
         myList.add("Elem 1");
         myList.add("Elem 2");
 
@@ -98,11 +113,14 @@ public class MyArrayListTests{
 
     @Test(expected = IndexOutOfBoundsException.class)
     public void testGetOutOfBounds(){
+        // Test getting an element at an out-of-bounds index, expecting an exception
         myList.get(0);
     }
 
     @Test
     public void testRemoveByValue(){
+
+        // Test removing an element by its value
         myList.add("Elem 1");
         myList.add("Elem 2");
 
@@ -116,6 +134,7 @@ public class MyArrayListTests{
 
     @Test
     public void testRemoveByValueNotFound(){
+        // Test removing a value that is not present in the list
         myList.add("Elem 1");
         myList.add("Elem 2");
 
@@ -125,6 +144,7 @@ public class MyArrayListTests{
 
     @Test
     public void testSet(){
+        // Test setting an element at a specific index
         myList.add("Elem 1");
         myList.add("Elem 2");
 
@@ -135,11 +155,13 @@ public class MyArrayListTests{
 
     @Test(expected = IndexOutOfBoundsException.class)
     public void testSetOutOfBounds() {
+        // Test setting an element at an out-of-bounds index, expecting an exception
         myList.set(0, "New Element");
     }
 
     @Test
     public void testIsEmpty(){
+        // Test the isEmpty method
         assertTrue(myList.isEmpty());
 
         myList.add("Elem 1");
@@ -149,6 +171,7 @@ public class MyArrayListTests{
 
     @Test
     public void testContains(){
+        // Test the contains method
         myList.add("Elem 1");
         myList.add("Elem 2");
 
@@ -158,6 +181,7 @@ public class MyArrayListTests{
 
     @Test
     public void testToArray(){
+        // Test converting the list to an array
         myList.add("Elem 1");
         myList.add("Elem 2");
 
@@ -167,12 +191,14 @@ public class MyArrayListTests{
 
     @Test
     public void testToArrayEmptyList(){
+        // Test converting an empty list to an array
         String[] array = myList.toArray(new String[0]);
         assertArrayEquals(new String[]{}, array);
     }
 
     @Test
     public void testIterator(){
+        // Test the iterator method
         myList.add("Elem 1");
         myList.add("Elem 2");
 
--- src/utilities/MyArrayList.java
+++ src/utilities/MyArrayList.java
@@ -5,46 +5,61 @@ import java.util.NoSuchElementException;
 
 
 
-// import utilities.Iterator;
+
+
+// Represents a dynamic array-based implementation of ListADT interface
 
 public class MyArrayList<E> implements ListADT<E>{
 
+    // Default capacity of the array
     private static final int DEFAULT_CAPACITY = 10;
+
+    // Internal array to store elements
     private Object[] array;
+
+    // Current size of the list
     private int size;
 
+    // Default constructor initializes the array with the default capacity and sets size to 0
     public MyArrayList(){
         this.array = new Object[DEFAULT_CAPACITY];
         this.size = 0;
     }
 
-
+    // Returns the current size of the list
     @Override
     public int size(){
         return size;
     }
 
+    // Clears the list by setting the size to 0
     @Override
     public void clear(){
         size = 0;
     }
 
+    // Adds an element at the specified index, shifting subsequent elements to the right
     @Override
     public boolean add(int index, E toAdd) throws NullPointerException, IndexOutOfBoundsException{
+        // Check for null element
         if(toAdd == null){
             throw new NullPointerException("Element to add cannot be null");
         }
-        
+
+        // Check for valid index range
         if(index < 0 || index > size){
             throw new IndexOutOfBoundsException("Chosen index is out of range");
         }
 
+        // Ensure capacity for the new element
         ensureCapacity(size + 1);
 
+        // Shift elements to the right to make space for the new element
         for(int i = size - 1; i >= index; i--){
             array[i + 1] = array[i];
         }
 
+        // Insert the new element at the specified index
         array[index] = toAdd;
         size++;
         return true;
@@ -52,27 +67,35 @@ public class MyArrayList<E> implements ListADT<E>{
     }
 
 
+    // Adds an element to the end of the list
     @Override
     public boolean add(E toAdd) throws NullPointerException{
+        // Check for null element
         if(toAdd == null){
             throw new NullPointerException("The element you want to add can't be null");
         }
 
+        // Ensure capacity for the new element
         ensureCapacity(size + 1);
 
+        // Add the new element at the end of the list
         array[size] = toAdd;
         size++;
         return true;
     }
 
+    // Adds all elements from the specified list to the end of this list
     @Override
     public boolean addAll(ListADT <? extends E> toAdd) throws NullPointerException{
+        // Check for null list
         if (toAdd == null){
             throw new NullPointerException("The list can't be null");
         }
 
+        // Ensure capacity for the new elements
         ensureCapacity(size + toAdd.size());
 
+        // Iterate through the elements of the specified list and add them to this list
         Iterator<? extends E> iterator = toAdd.iterator();
         while (iterator.hasNext()){
             add(iterator.next());
@@ -81,91 +104,117 @@ public class MyArrayList<E> implements ListADT<E>{
         return true;
     }
 
+
+    // Returns the element at the specified index
     @Override
-    @SuppressWarnings("unchecked") // ?
+    @SuppressWarnings("unchecked")
     public E get(int index) throws IndexOutOfBoundsException{
+        // Check for valid index range
         if(index < 0 || index >= size){
             throw new IndexOutOfBoundsException("Entered index out of range");
         }
 
+        // Return the element at the specified index
         return (E) array[index];
     }
 
+    // Removes the element at the specified index, shifting subsequent elements to the left
     @Override
     @SuppressWarnings("unchecked")
     public E remove(int index) throws IndexOutOfBoundsException{
+        // Check for valid index range
         if(index < 0 || index >= size){
             throw new IndexOutOfBoundsException("Entered index is out of range");
         }
 
+        // Retrieve the element to be removed
         E removedElement = (E) array[index];
 
+        // Shift elements to the left to fill the gap
         System.arraycopy(array, index+1, array, index, size - index - 1);
 
+        // Set the last element to null and decrement the size
         array[size - 1] = null;
         size --;
 
         return removedElement;
     }
 
+    // Removes the first occurrence of the specified element from the list
     @Override
-    @SuppressWarnings("unchecked")
     public E remove (E toRemove) throws NullPointerException{
+        // Check for null element
         if(toRemove == null){
             throw new NullPointerException("The element can't be null");
         }
 
+        // Iterate through the list to find and remove the specified element
         for(int i = 0; i < size; i++){
             if(toRemove.equals(array[i])){
                 return remove(i);
             }
         }
 
+        // Return null if the element was not found
         return null;
     }
 
+    // Replaces the element at the specified index with the specified element
     @Override
     public E set(int index, E toChange) throws NullPointerException, IndexOutOfBoundsException{
+        // Check for null element
         if(toChange == null){
             throw new NullPointerException("The element can't be null");
         }
 
+        // Check for valid index range
         if(index < 0 || index >= size){
             throw new IndexOutOfBoundsException("Index out of range");
         }
 
+        // Retrieve the previous element, replace it, and return the previous element
         E previousElement = get(index);
         array[index] = toChange;
         return previousElement;
     }
 
+    // Checks if the list is empty
     @Override
     public boolean isEmpty(){
         return size == 0;
     }
 
+    // Checks if the list contains the specified element
     @Override
     public boolean contains (E toFind) throws NullPointerException{
+        // Check for null element
         if(toFind == null){
             throw new NullPointerException("Element to find cannot be null");
         }
 
+        // Iterate through the list to find the specified element
         for(int i = 0; i < size; i++){
             if(toFind.equals(array[i])){
                 return true;
             }
         }
 
+        // Return false if the element was not found
         return false;
     }
 
+
+    // Copies the elements of the list to the specified array
     @Override
     @SuppressWarnings("unchecked")
     public E[] toArray(E[] toHold) throws NullPointerException{
+
+        // Check for null array
         if(toHold == null){
             throw new NullPointerException("The array to hold elements can't be null");
         }
 
+        // Resize the array if needed or clear its contents
         if(toHold.length < size){
             toHold = (E[]) new Object[size];
         }
@@ -173,26 +222,33 @@ public class MyArrayList<E> implements ListADT<E>{
             clearArray(toHold);
         }
 
+        // Copy elements from the list to the array
         System.arraycopy(array, 0, toHold, 0, size);
 
         return toHold;
     }
 
+
+    // Copies the elements of the list to a new array
     @Override
     public Object[] toArray(){
+        // Create a new array and copy elements from the list to it
         Object[] result = new Object[size];
         System.arraycopy(array, 0, result, 0, size);
         return result;
     }
 
+    // Returns an iterator over the elements in the list
     @Override
     public Iterator<E> iterator(){
         return new MyIterator();
     }
 
+    // Ensures that the internal array has sufficient capacity for the specified minimum capacity
     private void ensureCapacity(int minCapacity){
         int currentCapacity = array.length;
-        
+
+        // If the current capacity is not enough, double it or use the specified minimum capacity
         if (minCapacity > currentCapacity){
             int newCapacity = Math.max(currentCapacity * 2, minCapacity);
             Object[] newArray = new Object[newCapacity];
@@ -201,34 +257,45 @@ public class MyArrayList<E> implements ListADT<E>{
         }
     }
 
+    // Copies the elements from the source array to the destination array up to the specified length
     private static void coppyArray(Object[] src, Object[] dest, int length){
         for(int i = 0; i < length; i++){
             dest[i] = src[i];
         }
     }
 
+    // Clears the elements of the array by setting each element to null
     private static void clearArray(Object[] array){
         for(int i = 0; i < array.length; i++){
             array[i] = null;
         }
     }
 
+    // Inner class representing an iterator over the elements in the list
     private class MyIterator implements Iterator<E>{
 
+        // Index of the current element in the iteration
         private int currentIndex = 0;
+
+        // Flag indicating whether there is a next element
         private boolean hasNext = true;
 
+        // Checks if there is a next element in the iteration
         @Override
         public boolean hasNext(){
             return hasNext;
         }
 
+        // Returns the next element in the iteration
         @Override
+        @SuppressWarnings("unchecked")
         public E next(){
+            // Throw an exception if there is no next element
             if(!hasNext()){
                 throw new NoSuchElementException("No elements in the list");
             }
 
+            // Retrieve the next element, increment the index, and update the hasNext flag
             E nextElement = (E) array[currentIndex];
 
             currentIndex ++;
