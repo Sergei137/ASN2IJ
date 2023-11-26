ad09e9293a3299d28bacbe5cb806e1e8546ee153
Sergei137 <sm1.618@protonmail.com>
2023-11-25 19:15:45 -0700

    updates

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,18 +2,7 @@ package main;
 
 public class Main {
     public static void main(String[] args) {
-
-        // DELETE THIS BEFORE SUBMISSION
-        System.out.println(factorial1(5));
-
-    }
-
-    // DELETE THIS BEFORE SUBMISSION
-    static int factorial1 (int x) {
-        if (x == 1) {
-            return 1;
-        }
-        return x * factorial1(x - 1);
+        System.out.println("Hello World");
     }
 }
 
